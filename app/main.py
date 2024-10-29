a0bc9681dee900c8fc09087413bb2491641bee95
Rhana <159554135+RHAN4@users.noreply.github.com>
2024-10-29 14:02:43 +0000

    faltando ajustes

--- app/main.py
+++ app/main.py
@@ -9,19 +9,62 @@ def main():
     service = UsuarioService(repository)
 
     # Solicitando dados para o usuário
-    print("\n - Adicionar usuário -")
-    nome = input("Digite o seu nome: ")
-    email = input("Digite o seu email: ")
-    senha = input("Digite o seu senha: ")
 
-    service.criar_usuario(nome=nome, email=email, senha=senha)
-
-    # Listar todos os usuarios cadastrados
-    print("\n - Lista de todos os usuários cadastrados -")
-    lista_usuarios = service.listar_todos_usuarios()
-    for usuario in lista_usuarios:
-        print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+    # MENU DE OPÇÕES
+    def menu():
+        print("\t - MENU DE OPÇÕES - ")
+        print("""  \t1 - Adicionar usuário. 
+            2 - Pesquisar um usuário. 
+            3 - Atualizar dados de um usuário.
+            4 - Excluir usuário.
+            5 - Exibir todos os usuários cadastrados
+            0 - Sair.""")
         
+    while True:
+        menu()
+        opcao = int(input("\nDigite a opção desejada: "))
+
+        match(opcao):
+            case 1:
+                print("\n - Adicionando usuário -")
+                nome = input("Digite o seu nome: ")
+                email = input("Digite o seu email: ")
+                senha = input("Digite o seu senha: ")
+
+                service.criar_usuario(nome=nome, email=email, senha=senha)
+                break
+
+            case 2:
+                print("\n - Pesquisa por email -")
+                email = input("Digite o email desejado: ")
+                lista_email = repository.pesquisar_usuario_por_email(email=email)
+                for usuario in lista_email:
+                    print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+                break
+
+            case 3:
+                print("\n - Atualização de dados -")
+                break
+
+            case 4:
+                print("\n - Excluir usuário -")
+                nome = input("Digite o nome: ")
+                email = input("Digite o email: ")
+                senha = input("Digite o senha: ")
+
+                repository.excluir_usuario()
+                break
+
+            case 5:
+                print("\n - Exibir todos os usuários cadastrados -")
+                lista_usuarios = service.listar_todos_usuarios()
+                for usuario in lista_usuarios:
+                    print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+                break
+
+            case 0:
+                break
+
 if __name__ == "__main__":
     os.system("cls || clear")
     main()
