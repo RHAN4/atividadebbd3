49a340a339ffab30bd1063687b4abe039ab1be17
Rhana <159554135+RHAN4@users.noreply.github.com>
2024-10-29 11:42:03 +0000

    finalização da construção

--- app/config/database.py
+++ app/config/database.py
@@ -2,26 +2,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
+# Parâmetros para conexão com BD MySQL:
 db_user = "user"
 db_password = "user_password"
 db_host = "localhost"
 db_port = "3306"
 db_name = "meu_banco"
 
+# Endereço para conexão com BD MySQL:
 DATABASE_URL = f"mysql+pymeysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
+# Conectando ao banco de dados:
 db = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=db)
 session = Session()
 
+# Gerenciando sessão:
 @contextmanager
 def get_db():
     db = Session
     try:
         yield db
-        db.commit()
+        db.commit() # Se caso der certo, faz o commit
     except Exception as erro:
-        db.rollback()
-        raise erro
+        db.rollback() # Se caso der erro, desfaz a operação
+        raise erro # Lança a exceção, informando o erro
     finally:
-        db.close()
+        db.close() # Garante o fechamento da sessão
---
+++ app/main.py
@@ -0,0 +1,11 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.database import Session
+import os
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    # Solicitando dados para o usuário
