1099ebaf26304f51afb7e88280a235a2f50a0784
Rhana <159554135+RHAN4@users.noreply.github.com>
2024-11-04 13:55:26 +0000

    dando erros

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from models.usuario_model import Usuario
 from services.usuario_service import UsuarioService
 from repositories.usuario_repository import UsuarioRepository
 from config.database import Session
@@ -5,6 +6,7 @@ import os
 
 # MENU DE OPÇÕES
 def menu():
+    print("\n")
     print("\t - MENU DE OPÇÕES - ")
     print("""  \t1 - Adicionar usuário. 
         2 - Pesquisar um usuário. 
@@ -32,15 +34,16 @@ def main():
 
                 service.criar_usuario(nome=nome, email=email, senha=senha)
                 
-
             case 2:
                 print("\n - Pesquisa por email -")
-                email = input("Digite o email desejado: ")
-                lista_email = repository.pesquisar_usuario_por_email(email=email)
-                for usuario in lista_email:
-                    print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+                email_consulta = input("Digite o email desejado: ")
+                # consulta = repository.pesquisar_usuario_por_email(email=email_consulta)
+                consulta = repository.session.query(Usuario).filter_by(email = email_consulta).first()
+                if consulta:
+                     print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+                else:
+                    print("Usuário não encontrado")
                 
-
             case 3:
                 print("\n - Atualização de dados -")
                 
@@ -51,7 +54,7 @@ def main():
                 email = input("Digite o email: ")
                 senha = input("Digite o senha: ")
 
-                repository.excluir_usuario()
+                repository.excluir_usuario(usuario)
                 
 
             case 5:
@@ -59,8 +62,7 @@ def main():
                 lista_usuarios = service.listar_todos_usuarios()
                 for usuario in lista_usuarios:
                     print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
-                
-                
+
 
             case 0:
                 print("Saindo...")
